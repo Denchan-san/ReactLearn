9f236f51d2679fb91278cd875b773cb0f0480e86
Jokir <doner24072004@gmail.com>
2024-01-18 16:12:19 +0200

    added class User and showing up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const user1 = new User ("John Doe", 25, "john@example.com");
   return (
-    <div>Hello world</div>
+    <>
+      <h1>Hello chiki briki</h1>
+      <p>User: user1</p>
+      <p>Name: {user1.name}</p>
+      <p>Age: {user1.age}</p>
+      <p>User: {user1.email}</p>
+      <img className="User" 
+      src={user1.photoUrl}
+      alt={'Photo of ' + user1.name}
+      style={{
+        width: 90,
+        height: 90
+      }} 
+      />
+    </>
   );
 }
 
-export default App;
+class User {
+  constructor(name, age, email,photoUrl) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.photoUrl = 'https://i.imgur.com/UkgrtOf.jpeg';
+  }
+}
+
